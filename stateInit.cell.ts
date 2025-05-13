5ac67c87a6102d4f4def4d53d14b4e27110f2da2
notoriousisk <ishkineyev@mail.ru>
2025-05-13 05:19:00 +0300

    q

--- stateInit.cell.ts
+++ stateInit.cell.ts
@@ -1,5 +1,10 @@
 import { beginCell } from "@ton/core";
 
-const cell = beginCell().endCell();
+const cell = beginCell()
+    .storeBuffer(Buffer.from("My Awesome Token", "utf8"))
+    .storeBuffer(Buffer.from("MAT", "utf8"))
+    .storeUint(9, 8)
+    .storeAddress('EQCObIMiCLlWlEdT9FVPZWz5jAWocxqhu6bYzwtJzEkkhAsa')
+    .endCell();
 
 export default cell;
